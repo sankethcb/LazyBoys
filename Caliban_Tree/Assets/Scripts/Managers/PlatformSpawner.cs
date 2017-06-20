3e788b828f123b47d0c22c7865bf284f38f2008d
Sanketh <Sanketh@SANKETH-LAPTOP>
2017-06-20 22:25:20 +0530

    Basic Platform Spawning

---
+++ Caliban_Tree/Assets/Scripts/Managers/PlatformSpawner.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformSpawner : MonoBehaviour
+{
+
+    public GameObject testPlatform;
+    public float spawnTime;
+
+    void Start()
+    {
+        InvokeRepeating("SpawnPlatform", spawnTime, spawnTime);
+    }
+
+
+    void Update()
+    {
+
+    }
+
+    void SpawnPlatform()
+    {
+        Instantiate(testPlatform, new Vector3(0, 5f, 0), Quaternion.identity);
+        Instantiate(testPlatform, new Vector3(5f, 5f, 0), Quaternion.identity);
+        Instantiate(testPlatform, new Vector3(-5f, 5f, 0), Quaternion.identity);
+    }
+
+    
+}
---
+++ Caliban_Tree/Assets/Scripts/Platforms/Platform.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Sanketh Bhat
+/// Parent Class for all Platform objects
+/// </summary>
+
+[RequireComponent(typeof(Rigidbody2D))]
+public abstract class Platform : MonoBehaviour
+{
+
+    //temp variable for movepseed
+
+    public int moveSpd;
+    Rigidbody2D rb2D;
+
+	void Start ()
+    {
+        rb2D = GetComponent<Rigidbody2D>();
+        rb2D.velocity = new Vector2(0, -moveSpd);
+    }
+	
+	
+	void Update ()
+    {
+        if (transform.position.y < -10f)
+            Destroy(gameObject);
+	}
+
+    void OnBecameInVisible()
+    {
+        enabled = false;
+
+    }
+
+    void OnBecameVisible()
+    {
+        enabled = true;
+    }
+}
